fbce62f42504d89ec0d99c81042b35947e2437cf
juarisco <juarisco@outlook.com>
2019-02-24 20:32:15 -0500

    Validar el formulario de productos de nuestra administración

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->slug = str_slug($product->name);
+        });
+    }
 }
